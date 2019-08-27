d8144495ba05c97cff5790850cfc9be227dcb1c0
Daniel (Yunbing) Huang <yunbing.huang@capitalone.com>
2019-08-27 10:24:37 -0400

    update dependency version

--- src/main/java/com/capitalone/dashboard/config/WebSecurityConfig.java
+++ src/main/java/com/capitalone/dashboard/config/WebSecurityConfig.java
@@ -89,7 +89,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.POST, "/generic-item").permitAll()
                 .antMatchers(HttpMethod.POST, "/generic-binary-artifact").permitAll()
                 .antMatchers(HttpMethod.POST, "/metadata/create").permitAll()
-                .antMatchers(HttpMethod.POST, "/autodiscovery/updateStatus").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .addFilterBefore(standardLoginRequestFilter(), UsernamePasswordAuthenticationFilter.class)
