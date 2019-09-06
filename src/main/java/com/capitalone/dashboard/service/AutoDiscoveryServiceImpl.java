08614922db72568ad8a7d0e0f98984ea6487653f
Daniel (Yunbing) Huang <yunbing.huang@capitalone.com>
2019-09-06 18:29:57 -0400

    When updating an existing AutoDiscovery record, only update the status, not any other fields. Created unit tests for create/update happy paths, and exception handling when updating existing records. update core version to 3.1.5

--- src/main/java/com/capitalone/dashboard/service/AutoDiscoveryServiceImpl.java
+++ src/main/java/com/capitalone/dashboard/service/AutoDiscoveryServiceImpl.java
@@ -1,6 +1,7 @@
 package com.capitalone.dashboard.service;
 
 import com.capitalone.dashboard.misc.HygieiaException;
+import com.capitalone.dashboard.model.AutoDiscoveredEntry;
 import com.capitalone.dashboard.model.AutoDiscovery;
 import com.capitalone.dashboard.repository.AutoDiscoveryRepository;
 import com.capitalone.dashboard.model.AutoDiscoveryRemoteRequest;
@@ -10,6 +11,7 @@ import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 
 @Service
 public class AutoDiscoveryServiceImpl implements AutoDiscoveryService {
@@ -23,18 +25,23 @@ public class AutoDiscoveryServiceImpl implements AutoDiscoveryService {
 
     @Override
     public AutoDiscovery save(AutoDiscoveryRemoteRequest request) throws HygieiaException {
-        final String METHOD_NAME = "AutoDiscoveryServiceImpl.save";
         String autoDiscoveryId = request.getAutoDiscoveryId();
-        if (! ObjectId.isValid(autoDiscoveryId) ) {
-            throw new HygieiaException("Invalid Auto Discovery Object ID: " + autoDiscoveryId + " received.", HygieiaException.BAD_DATA);
+        if (autoDiscoveryId==null || !ObjectId.isValid(autoDiscoveryId)) {
+            throw new HygieiaException("Invalid Auto Discovery Object ID: [" + autoDiscoveryId + "] received.", HygieiaException.BAD_DATA);
         }
 
-        AutoDiscovery autoDiscovery = requestToAutoiscovery( request );
         ObjectId id = new ObjectId(autoDiscoveryId);
+        AutoDiscovery autoDiscovery = null;
 
         if (autoDiscoveryRepository.exists(id)) {
-            autoDiscovery.setId(id);
+            // update existing AutoDiscovery record with the status from request
+            autoDiscovery = autoDiscoveryRepository.findOne(id);
+            updateAutoiscovery(autoDiscovery, request);
+        } else {
+            // create new AutoDiscovery record
+            autoDiscovery = requestToAutoiscovery(request);
         }
+
         autoDiscoveryRepository.save(autoDiscovery);
         return autoDiscovery;
     }
@@ -49,4 +56,38 @@ public class AutoDiscoveryServiceImpl implements AutoDiscoveryService {
                 request.getDeploymentEntries(), request.getLibraryScanEntries(), request.getFunctionalTestEntries(), request.getArtifactEntries(),
                 request.getStaticCodeEntries(), request.getFeatureEntries());
     }
+
+
+    /**
+     * Update the AutoDiscovery Entries' status from the request.
+     * @param autoDiscovery
+     * @param request
+     */
+    private void updateAutoiscovery(AutoDiscovery autoDiscovery, AutoDiscoveryRemoteRequest request) {
+        updateEntryStatus(request.getCodeRepoEntries(), autoDiscovery.getCodeRepoEntries());
+        updateEntryStatus(request.getBuildEntries(), autoDiscovery.getBuildEntries());
+        updateEntryStatus(request.getSecurityScanEntries(), autoDiscovery.getSecurityScanEntries());
+        updateEntryStatus(request.getDeploymentEntries(), autoDiscovery.getDeploymentEntries());
+        updateEntryStatus(request.getLibraryScanEntries(), autoDiscovery.getLibraryScanEntries());
+        updateEntryStatus(request.getFunctionalTestEntries(), autoDiscovery.getFunctionalTestEntries());
+        updateEntryStatus(request.getArtifactEntries(), autoDiscovery.getArtifactEntries());
+        updateEntryStatus(request.getStaticCodeEntries(), autoDiscovery.getStaticCodeEntries());
+        updateEntryStatus(request.getFeatureEntries(), autoDiscovery.getFeatureEntries());
+    }
+
+    /**
+     * Update the AutoDiscovery Entries' status from source to target.
+     * @param source
+     * @param target
+     */
+    private void updateEntryStatus(List<AutoDiscoveredEntry> source, List<AutoDiscoveredEntry> target) {
+        for (AutoDiscoveredEntry srcEntry : source) {
+            target.stream().forEach(entry -> {
+                if(entry.getOptions().equals(srcEntry.getOptions())){
+                    entry.setStatus(srcEntry.getStatus());
+                }
+            });
+        }
+    }
+
 }
