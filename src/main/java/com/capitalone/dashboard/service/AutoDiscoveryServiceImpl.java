abb5540dd95c9feb00498b0121f878c00672c956
rvema <ragha.vema@capitalone.com>
2019-11-22 23:06:26 -0500

    auto discovery fixes

--- src/main/java/com/capitalone/dashboard/service/AutoDiscoveryServiceImpl.java
+++ src/main/java/com/capitalone/dashboard/service/AutoDiscoveryServiceImpl.java
@@ -3,16 +3,23 @@ package com.capitalone.dashboard.service;
 import com.capitalone.dashboard.misc.HygieiaException;
 import com.capitalone.dashboard.model.AutoDiscoveredEntry;
 import com.capitalone.dashboard.model.AutoDiscovery;
-import com.capitalone.dashboard.model.Collector;
-import com.capitalone.dashboard.repository.AutoDiscoveryRepository;
 import com.capitalone.dashboard.model.AutoDiscoveryRemoteRequest;
+import com.capitalone.dashboard.model.Collector;
+import com.capitalone.dashboard.model.CollectorType;
+import com.capitalone.dashboard.model.FeatureFlag;
+import com.capitalone.dashboard.repository.AutoDiscoveryRepository;
 import com.capitalone.dashboard.repository.CollectorRepository;
+import com.capitalone.dashboard.repository.FeatureFlagRepository;
+import com.capitalone.dashboard.util.FeatureFlagsEnum;
+import com.capitalone.dashboard.util.HygieiaUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -21,11 +28,13 @@ public class AutoDiscoveryServiceImpl implements AutoDiscoveryService {
     private static final Log LOG = LogFactory.getLog(AutoDiscoveryServiceImpl.class);
     private final AutoDiscoveryRepository autoDiscoveryRepository;
     private final CollectorRepository collectorRepository;
+    private final FeatureFlagRepository featureFlagRepository;
 
     @Autowired
-    public AutoDiscoveryServiceImpl(AutoDiscoveryRepository autoDiscoveryRepository,CollectorRepository collectorRepository) {
+    public AutoDiscoveryServiceImpl(AutoDiscoveryRepository autoDiscoveryRepository,CollectorRepository collectorRepository, FeatureFlagRepository featureFlagRepository) {
         this.autoDiscoveryRepository = autoDiscoveryRepository;
         this.collectorRepository = collectorRepository;
+        this.featureFlagRepository = featureFlagRepository;
     }
 
     @Override
@@ -37,15 +46,16 @@ public class AutoDiscoveryServiceImpl implements AutoDiscoveryService {
 
         ObjectId id = new ObjectId(autoDiscoveryId);
         AutoDiscovery autoDiscovery = null;
+        FeatureFlag featureFlag = featureFlagRepository.findByName(FeatureFlagsEnum.auto_discover.toString());
 
         if (autoDiscoveryRepository.exists(id)) {
             // update existing AutoDiscovery record with the status from request
             autoDiscovery = autoDiscoveryRepository.findOne(id);
-            updateAutoiscovery(autoDiscovery, request);
+            updateAutoDiscovery(autoDiscovery, request, featureFlag);
             autoDiscovery.setModifiedTimestamp(System.currentTimeMillis());
         } else {
             // create new AutoDiscovery record
-            autoDiscovery = requestToAutoiscovery(request);
+            autoDiscovery = requestToAutoiscovery(request, featureFlag);
             long currTime = System.currentTimeMillis();
             autoDiscovery.setCreatedTimestamp(currTime);
             autoDiscovery.setModifiedTimestamp(currTime);
@@ -60,7 +70,9 @@ public class AutoDiscoveryServiceImpl implements AutoDiscoveryService {
      * @param request
      * @return AutoDiscovery
      */
-    private AutoDiscovery requestToAutoiscovery(AutoDiscoveryRemoteRequest request) {
+    private AutoDiscovery requestToAutoiscovery(AutoDiscoveryRemoteRequest request, FeatureFlag featureFlag) {
+
+        cleanAutoDiscoveryRequestByFeatureFlag(request, featureFlag);
         return new AutoDiscovery(request.getMetaData(), request.getCodeRepoEntries(), request.getBuildEntries(), request.getSecurityScanEntries(),
                 request.getDeploymentEntries(), request.getLibraryScanEntries(), request.getFunctionalTestEntries(), request.getArtifactEntries(),
                 request.getStaticCodeEntries(), request.getFeatureEntries());
@@ -72,7 +84,8 @@ public class AutoDiscoveryServiceImpl implements AutoDiscoveryService {
      * @param autoDiscovery
      * @param request
      */
-    private void updateAutoiscovery(AutoDiscovery autoDiscovery, AutoDiscoveryRemoteRequest request) {
+    private void updateAutoDiscovery(AutoDiscovery autoDiscovery, AutoDiscoveryRemoteRequest request, FeatureFlag featureFlag) {
+
         updateEntryStatus(request.getCodeRepoEntries(), autoDiscovery.getCodeRepoEntries());
         updateEntryStatus(request.getBuildEntries(), autoDiscovery.getBuildEntries());
         updateEntryStatus(request.getSecurityScanEntries(), autoDiscovery.getSecurityScanEntries());
@@ -82,6 +95,31 @@ public class AutoDiscoveryServiceImpl implements AutoDiscoveryService {
         updateEntryStatus(request.getArtifactEntries(), autoDiscovery.getArtifactEntries());
         updateEntryStatus(request.getStaticCodeEntries(), autoDiscovery.getStaticCodeEntries());
         updateEntryStatus(request.getFeatureEntries(), autoDiscovery.getFeatureEntries());
+        removeEntriesByFeatureFlag(autoDiscovery, featureFlag);
+    }
+
+    private void removeEntriesByFeatureFlag(@NotNull AutoDiscovery autoDiscovery, FeatureFlag featureFlag) {
+        if(!HygieiaUtils.allowAutoDiscover(featureFlag, CollectorType.SCM)) autoDiscovery.setCodeRepoEntries(new ArrayList<>());
+        if(!HygieiaUtils.allowAutoDiscover(featureFlag, CollectorType.Build)) autoDiscovery.setBuildEntries(new ArrayList<>());
+        if(!HygieiaUtils.allowAutoDiscover(featureFlag, CollectorType.StaticSecurityScan)) autoDiscovery.setSecurityScanEntries(new ArrayList<>());
+        if(!HygieiaUtils.allowAutoDiscover(featureFlag, CollectorType.Deployment)) autoDiscovery.setDeploymentEntries(new ArrayList<>());
+        if(!HygieiaUtils.allowAutoDiscover(featureFlag, CollectorType.LibraryPolicy)) autoDiscovery.setLibraryScanEntries(new ArrayList<>());
+        if(!HygieiaUtils.allowAutoDiscover(featureFlag, CollectorType.Test)) autoDiscovery.setFunctionalTestEntries(new ArrayList<>());
+        if(!HygieiaUtils.allowAutoDiscover(featureFlag, CollectorType.Artifact)) autoDiscovery.setArtifactEntries(new ArrayList<>());
+        if(!HygieiaUtils.allowAutoDiscover(featureFlag, CollectorType.CodeQuality)) autoDiscovery.setStaticCodeEntries(new ArrayList<>());
+        if(!HygieiaUtils.allowAutoDiscover(featureFlag, CollectorType.AgileTool)) autoDiscovery.setFeatureEntries(new ArrayList<>());
+    }
+
+    private void cleanAutoDiscoveryRequestByFeatureFlag(@NotNull AutoDiscoveryRemoteRequest request, FeatureFlag featureFlag){
+        if(!HygieiaUtils.allowAutoDiscover(featureFlag, CollectorType.SCM)) request.setCodeRepoEntries(new ArrayList<>());
+        if(!HygieiaUtils.allowAutoDiscover(featureFlag, CollectorType.Build)) request.setBuildEntries(new ArrayList<>());
+        if(!HygieiaUtils.allowAutoDiscover(featureFlag, CollectorType.StaticSecurityScan)) request.setSecurityScanEntries(new ArrayList<>());
+        if(!HygieiaUtils.allowAutoDiscover(featureFlag, CollectorType.Deployment)) request.setDeploymentEntries(new ArrayList<>());
+        if(!HygieiaUtils.allowAutoDiscover(featureFlag, CollectorType.LibraryPolicy)) request.setLibraryScanEntries(new ArrayList<>());
+        if(!HygieiaUtils.allowAutoDiscover(featureFlag, CollectorType.Test)) request.setFunctionalTestEntries(new ArrayList<>());
+        if(!HygieiaUtils.allowAutoDiscover(featureFlag, CollectorType.Artifact)) request.setArtifactEntries(new ArrayList<>());
+        if(!HygieiaUtils.allowAutoDiscover(featureFlag, CollectorType.CodeQuality)) request.setStaticCodeEntries(new ArrayList<>());
+        if(!HygieiaUtils.allowAutoDiscover(featureFlag, CollectorType.AgileTool)) request.setFeatureEntries(new ArrayList<>());
     }
 
     /**
