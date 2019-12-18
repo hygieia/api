b2a5fb9f90870ba12841a3f1ce0323c79d81279e
Murugan, Aravindhan <aravindhan.murugan@capitalone.com>
2019-12-18 14:52:04 -0500

    validated annotation added to controller

--- src/main/java/com/capitalone/dashboard/webhook/sonarqube/SonarQubeHookController.java
+++ src/main/java/com/capitalone/dashboard/webhook/sonarqube/SonarQubeHookController.java
@@ -7,6 +7,7 @@ import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,7 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @RestController
+@Validated
 @RequestMapping("/webhook")
 public class SonarQubeHookController {
     private final SonarQubeHookService sonarQubeHookService;
