06e59b6fc6bf57faa1a0e7d70a0763250d515f6c
nireeshT <nireesh.thiruveedula@capitalone.com>
2020-04-01 00:17:52 -0400

    PerformanceEntry in DashboardRemoteRequest (#124)
    * Data cleanup api
    
    * Data cleanup api
    
    * PerformanceTestEntry in DashboardRemoteRequest
    Datasync to update test collectorItems with testType
    
    * LibraryPolicyThreatLevel changes - Add maxscore

--- src/main/java/com/capitalone/dashboard/request/DashboardRemoteRequest.java
+++ src/main/java/com/capitalone/dashboard/request/DashboardRemoteRequest.java
@@ -49,6 +49,9 @@ public class DashboardRemoteRequest {
     @Valid
     private List<ArtifactEntry> artifactEntries = new ArrayList<>();
 
+    @Valid
+    private List<PerformanceTestEntry> performanceTestEntries = new ArrayList<>();
+
     /**
      * Dashboard Metadata
      */
@@ -455,6 +458,34 @@ public class DashboardRemoteRequest {
         }
     }
 
+    /**
+     * Entry to create Functional Test in Code Quality Widget
+     */
+    public static class PerformanceTestEntry extends Entry {
+
+        @Override
+        public CollectorType getType() {
+            return CollectorType.Test;
+        }
+
+        @Override
+        public String getWidgetId() {
+            return "codeanalysis0";
+        }
+
+        @Override
+        public String getWidgetName() {
+            return "codeanalysis";
+        }
+
+        @Override
+        public Map<String, Object> toWidgetOptions() {
+            Map<String, Object> opts = new HashMap<>();
+            opts.put("id", getWidgetId());
+            return opts;
+        }
+    }
+
 
     /**
      * Entry to create Functional Test in Code Quality Widget
@@ -596,6 +627,15 @@ public class DashboardRemoteRequest {
         this.artifactEntries = artifactEntries;
     }
 
+    public List<PerformanceTestEntry> getPerformanceTestEntries() {
+        return performanceTestEntries;
+    }
+
+    public void setPerformanceTestEntries(List<PerformanceTestEntry> performanceTestEntries) {
+        this.performanceTestEntries = performanceTestEntries;
+    }
+
+
     public List<Entry> getAllEntries() {
         List<Entry> all = new ArrayList<>();
         all.addAll(buildEntries);
@@ -607,6 +647,7 @@ public class DashboardRemoteRequest {
         all.addAll(deploymentEntries);
         all.addAll(featureEntries);
         all.addAll(artifactEntries);
+        all.addAll(performanceTestEntries);
         return all;
     }
 }
