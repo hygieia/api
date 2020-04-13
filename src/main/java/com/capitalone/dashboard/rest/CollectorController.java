d5ec7249935dcc023fabfd2fd992c958f2c23901
nireeshT <nireesh.thiruveedula@capitalone.com>
2020-04-13 10:46:07 -0400

    getAllCollectorItems by collectorType and searchField (#126)
    * getAllCollectorItems by collectorType and searchField
    
    * getAllCollectorItems by collectorType and searchField
    
    * Updated core version to 3.6.2
    
    * updated javadocs - LGM alerts
    updated core - 3.6.3
    
    * updated javadocs - LGM alerts

--- src/main/java/com/capitalone/dashboard/rest/CollectorController.java
+++ src/main/java/com/capitalone/dashboard/rest/CollectorController.java
@@ -125,4 +125,15 @@ public class CollectorController {
                 .body(pageOfCollectorItems.getContent());
     }
 
+    @RequestMapping(value = "/collector/item/type/searchField/{collectorType}", method = GET,
+            produces = APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<CollectorItem>> collectorItemsByTypeAndSearchField(@PathVariable CollectorType collectorType, @RequestParam(value = "search", required = false, defaultValue = "") String descriptionFilter,@RequestParam(value = "searchField", required = false, defaultValue = "") String searchField, @PageableDefault(size = Integer.MAX_VALUE) Pageable pageable) {
+        Page<CollectorItem> pageOfCollectorItems = collectorService.collectorItemsByTypeWithFilter(collectorType, descriptionFilter,searchField, pageable);
+        return ResponseEntity
+                .ok()
+                .headers(paginationHeaderUtility.buildPaginationHeaders(pageOfCollectorItems))
+                .body(pageOfCollectorItems.getContent());
+    }
+
+
 }
