3c47941753b26a93c9ef9df29bad249fa167f5f3
danielyhuang <yunbing.huang@capitalone.com>
2020-04-22 11:14:51 -0400

    update import

--- src/main/java/com/capitalone/dashboard/auth/token/JwtAuthenticationFilter.java
+++ src/main/java/com/capitalone/dashboard/auth/token/JwtAuthenticationFilter.java
@@ -1,4 +1,10 @@
 package com.capitalone.dashboard.auth.token;
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
@@ -9,11 +15,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @Component
 @Order(2)
