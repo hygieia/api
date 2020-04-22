89e1dd6482120ba0c79b056d35c5bd82a62b8ccd
danielyhuang <yunbing.huang@capitalone.com>
2020-04-21 21:25:02 -0400

    Improve API Usage Logging

--- src/main/java/com/capitalone/dashboard/auth/token/JwtAuthenticationFilter.java
+++ src/main/java/com/capitalone/dashboard/auth/token/JwtAuthenticationFilter.java
@@ -1,6 +1,6 @@
 package com.capitalone.dashboard.auth.token;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.annotation.Order;
