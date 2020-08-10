6561341ec5d3227a019ed91d7d2d980469eb373d
Benjamin Xu <jiyang.xu@capitalone.com>
2020-08-10 17:01:22 -0400

    Fix review comments

--- src/test/java/com/capitalone/dashboard/service/DashboardRemoteServiceTest.java
+++ src/test/java/com/capitalone/dashboard/service/DashboardRemoteServiceTest.java
@@ -39,7 +39,11 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {ApiTestConfig.class, FongoConfig.class})
