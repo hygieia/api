bf20c9bfc61f7829b56eb365c05b005ba16f8f50
nameisaravind <nameisaravind@gmail.com>
2021-05-13 14:24:46 -0400

    cleanup

--- src/main/java/com/capitalone/dashboard/service/InfraStructureServiceImpl.java
+++ src/main/java/com/capitalone/dashboard/service/InfraStructureServiceImpl.java
@@ -4,8 +4,6 @@ import com.capitalone.dashboard.model.InfrastructureScan;
 import com.capitalone.dashboard.model.DataResponse;
 import com.capitalone.dashboard.repository.InfrastructureScanRepository;
 import com.capitalone.dashboard.request.InfraStructureRequest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,8 +11,6 @@ import java.util.Collections;
 
 @Service
 public class InfraStructureServiceImpl implements InfraStructureService {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(InfraStructureServiceImpl.class);
 
     private final InfrastructureScanRepository infrastructureScanRepository;
 
