5c7043a732156fc48b7a8afe8b82dae027d2cddb
dcanar9 <anthony.canar@hotmail.com>
2022-05-10 06:51:02 -0700

    made page size a value from the properties file

--- src/main/java/com/capitalone/dashboard/service/DashboardServiceImpl.java
+++ src/main/java/com/capitalone/dashboard/service/DashboardServiceImpl.java
@@ -36,6 +36,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -81,6 +82,8 @@ public class DashboardServiceImpl implements DashboardService {
     public static final String TEST = "test";
     public static final String CODEANALYSIS = "codeanalysis";
     public static final String INFRA_SCAN = "infrascan";
+    @Value("${pageSize:500}")
+    private int pageSize;
 
     @Autowired
     private ApiSettings settings;
@@ -1008,9 +1011,10 @@ public class DashboardServiceImpl implements DashboardService {
 
     @Override
     public String removeWidgetDuplicatesHelper(String title, boolean dryRun){
-        // page results and clean until there are no more pages
+        System.out.println("Pages of size: " + String.valueOf(pageSize));
+        // get page and clean until there are no more pages
         if(StringUtils.isEmpty(title)){
-            Pageable pageable = new PageRequest(0, 1000);
+            Pageable pageable = new PageRequest(0, pageSize);
             Page<Dashboard> page = findDashboardsByPage("", pageable);
 
             while(page.hasNext()){
